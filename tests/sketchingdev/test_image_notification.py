7c07cdbb2de58d3a9b2900a13344074e49c3dfee
SketchingDev <SketchingDev@users.noreply.github.com>
2018-08-05 17:36:22 +0100

    Initial functionality

--- tests/sketchingdev/test_image_notification.py
+++ tests/sketchingdev/test_image_notification.py
@@ -1,3 +1,7 @@
+from os import path
+
+import os
+
 import click
 import unittest
 from click.testing import CliRunner
@@ -9,15 +13,25 @@ from sketchingdev.console import ConsoleDisplay
 class TestConsoleDisplayWithImages(unittest.TestCase):
 
     def test_draw_image(self):
-        image_path = "/path/to/image"
-        console_output = "Image: /path/to/image"
+        data_dir = path.join(TestConsoleDisplayWithImages._get_current_directory(), "data/")
+        notification_image = path.join(data_dir, "image.bmp")
 
         image_notification = ImageNotification()
-        image_notification.set_image_path(image_path)
-        cmd = create_cmd(lambda: ConsoleDisplay().draw(image_notification))
+        image_notification.set_image_path(notification_image)
+        cmd = create_cmd(lambda: ConsoleDisplay((100, 100)).draw(image_notification))
         result = CliRunner().invoke(cmd, catch_exceptions=False)
 
-        self.assertEqual(console_output, result.output)
+        f = open(path.join(data_dir, "image.txt"), 'w')
+        f.write(result.output)
+        f.close()
+
+        self.maxDiff = None
+        self.assertEqual("", result.output)
+
+    @staticmethod
+    def _get_current_directory():
+        return os.path.dirname(os.path.realpath(__file__))
+
 
 
 def create_cmd(func):
